1bf60d59f71c228a63b6248a13bb7a21b919fb66
Nicholas Lofton <nick.lofton@angieslist.com>
2016-12-07 21:00:56 -0500

    Adding back executable

---
+++ cmake-build-debug/CMakeFiles/FuzzyClusteringNN-Limb.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/main.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/Neuron.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/Network.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/TrainingData.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/ClusterDataSet.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/Cluster.cpp.o"
+  "CMakeFiles/FuzzyClusteringNN-Limb.dir/Signal.cpp.o"
+  "FuzzyClusteringNN-Limb.pdb"
+  "FuzzyClusteringNN-Limb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FuzzyClusteringNN-Limb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
